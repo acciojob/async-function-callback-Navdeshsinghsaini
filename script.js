0184fe3806d2591ca2c76d78cb5cbee2c9251ac9
Navdeshsinghsaini <naveshsinghsaini@gmail.com>
2023-03-18 01:09:53 +0530

    second1 commit

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+const placeHolder = document.getElementById("output");
+const butt = document.getElementById("btn");
+const hell = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+async function getData() {
+    const url =  await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const data = await url.json();
+    // console.log (data);
+    // console.log (data.title);
+    placeHolder.innerText = data.title ;
+}
+
+  butt.addEventListener('click' , getData);
